63e524824e2a49d4fe89f3bfeaf94526b9505710
Sofia Nadeem <sofia.nadeem@live.com>
2020-02-10 18:52:08 -0700

    Argument in a function

--- ch04-function/02simpleWithArg.py
+++ ch04-function/02simpleWithArg.py
@@ -1,18 +1,19 @@
 
-def line():
-    print("-------------")
+# line() displays a line followed by message.
+def line(message):
+    print("-------------", message)
 
 name = input("Enter your name: ")
 print("Welcome", name)
-line()
+line("Welcome message displayed.")
 
 number = int(input("Enter a number for table (0 to exit): "))
 while number != 0:
-    line()
+    line(f"{number} read as keyboard input.")
     for count in range(1,11):
         print(f"{number} x {count} = {count * number}")
-    line()
+    line("table displayed.")
     number = int(input("Enter a number for table (0 to exit): "))
 
-line()
+line("A ZERO was enterred to end this program")
 print("Thank you")
