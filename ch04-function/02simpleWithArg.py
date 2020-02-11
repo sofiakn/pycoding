f290646ee0ec07a4a9a8d061a08cd3980017c0a0
Sofia Nadeem <sofia.nadeem@live.com>
2020-02-10 21:28:12 -0700

    Function that returns value

--- ch04-function/02simpleWithArg.py
+++ ch04-function/02simpleWithArg.py
@@ -2,18 +2,21 @@
 # line() displays a line followed by message.
 def line(message):
     print("-------------", message)
+    
+def read():
+    return int(input("Enter a number for table (0 to exit): "))
 
 name = input("Enter your name: ")
 print("Welcome", name)
 line("Welcome message displayed.")
 
-number = int(input("Enter a number for table (0 to exit): "))
+number = read()
 while number != 0:
     line(f"{number} read as keyboard input.")
     for count in range(1,11):
         print(f"{number} x {count} = {count * number}")
     line("table displayed.")
-    number = int(input("Enter a number for table (0 to exit): "))
+    number = read()
 
 line("A ZERO was enterred to end this program")
 print("Thank you")
